5cae8f679068642472de39f8810e004892951e2a
estevanmaito <ejmaito@gmail.com>
2016-01-08 14:17:40 -0200

    change client env var

--- app/controllers/shortenerHandler.client.js
+++ app/controllers/shortenerHandler.client.js
@@ -4,7 +4,7 @@
 	var sendButton = document.querySelector('.btn-send');
 	var urlInput = document.querySelector('.long-url');
 	var resultingUrl = document.querySelector('.resulting-url');
-	var apiUrl = process.env.APP_URL;
+	var apiUrl = 'https://ushor.herokuapp.com/';
 
 	function ajaxRequest (method, url, callback) {
 		var http = new XMLHttpRequest();
